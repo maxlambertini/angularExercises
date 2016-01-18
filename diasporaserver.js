81a77b9d2c5416edd72487ce44f0c97ff3785d46
Max Lambertini <m.lambertini@gmail.com>
2016-01-18 10:03:50 +0100

    Committai roba

--- diasporaserver.js
+++ diasporaserver.js
@@ -22,11 +22,83 @@ console.log ("Body parser installed");
 
 var router = express.Router();
 
-router.get("/diaspora/:id",function(req,res) {
-	var id = req.params.id;
-	console.log ("READ " + id);
-	res.json ({"id" : id})	
-});
+router.get ("/diaspora/clusterNames",function(req,res) {
+	var view = "_design/clusters/_view/clusterNames";
+	var db = "diaspora";
+	var options = {
+		startKey : [],
+		endKey : []
+	};
+	couch.get (db, view, options, function(err,resData) {
+		if (err) {
+			console.log ("KO")
+			console.log (err);
+			console.log (resData);
+			res.json ({status: "ERROR", value: err, code: resData});
+		}
+		else {
+			console.log ("OK");
+			console.log (resData);
+			if (resData.data && resData.data.rows) {
+				res.json (resData.data.rows);
+			} else {
+				res.json ({status: "ERROR", value: "Error fetching rows"});
+			}
+		}
+	});
+}),
+
+router.route('/diaspora/:id/:rev')
+	.get(function(req,res) {
+		var id = req.params.id;
+		console.log ("READ " + id);
+		couch.get ("diaspora",id, function (err,resData) {
+			if (err) {
+				console.log ("KO")
+				console.log (err);
+				console.log (resData);
+				res.json ({status: "ERROR", value: err, code: resData});
+			}
+			else {
+				console.log ("OK");
+				console.log (resData);
+				res.json(resData.data);
+			}
+
+		})
+	})
+	.delete (function (req,res) {
+		console.log ("DELETE /diaspora");
+		var d = new diaspora.Diaspora();
+		var p = req.params;
+		//console.log (p);
+		console.log (req.params);
+		couch.del ("diaspora", p.id, p.rev, function (err, resData) {
+			console.log ("Tried update, result is:");
+			console.log (resData);
+			res.type("application/json");
+			if (err) {
+				console.log ("Got error ")
+				console.log (err)
+				res.json ({
+					status: "KO",
+					error : err
+				})
+			} else {
+				res.json ({
+					status: "OK",
+					resData : resData
+				});					
+			}
+			console.log("Done");			
+		})	
+		console.log(p);
+		//res.json(d);
+		
+	});
+
+
+
 
 router	
 	.route("/diaspora")
@@ -66,13 +138,47 @@ router
 		console.log(p);
 		//res.json(d);
 		
+	})
+	.put (function (req,res) {
+		console.log ("PUT /diaspora");
+		//console.log ("Calling postme");
+		var d = new diaspora.Diaspora();
+		//console.log (req.body);
+		var p = req.body;
+		//console.log (p);
+		console.log (p);
+		couch.update ("diaspora", p, function (err, resData) {
+			console.log ("Tried update, result is:");
+			console.log (resData);
+			res.type("application/json");
+			if (err) {
+				console.log ("Got error ")
+				console.log (err)
+				res.json ({
+					status: "KO",
+					error : err
+				})
+			} else {
+				res.json ({
+					status: "OK",
+					resData : resData
+				});					
+			}
+			console.log("Done");			
+		})	
+		console.log(p);
+		//res.json(d);
+		
 	});
+
+
 
 console.log ("Routes created");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
--- js/diasporaApp.js
+++ js/diasporaApp.js
@@ -1,19 +1,142 @@
 var diasporaApp = angular.module("diasporaApp",[])
-	.controller("diasporaCtrl", function ($scope, $http) {
+	.controller("diasporaCtrl", function ($scope, $http, $timeout) {
 		$scope.cluster = null;	
 		$scope.currentNode = null;
+		$scope.clusterNames = null;
+
+		$scope.BASEURL = 'http://localhost:41459/diaspora';
+		$scope.DBURLNAMES = 'http://localhost:5984/diaspora/_design/clusters/_view/clusterNames';
+		$scope.DBURLDIASPORA = 'http://localhost:5984';
+
+		$scope.editingClusterName = false;
+		$scope.editingAspects = false;
+		$scope.showClusterNamesDlg = false;
+
+		$scope.selectedClusterId = null;
+
+		$scope.showLoadClusterDialog = function() { 
+			$("#loadClusters").modal('show');
+		}
+
+		$scope.hideLoadClusterDialog = function() { $scope.showClusterNamesDlg = false;}
+
+
 		
-		$scope.clusterEnabled = function() { return $scope.cluster != null; }		
 		$scope.currentNodeEnabled = function() { return $scope.currentNode != null; }
 		$scope.linkedToEnabled = function() { return $scope.currentNode != null && $scope.currentNode.linkedTo.length > 0; }
 		$scope.linkedFromEnabled = function() { return $scope.currentNode != null && $scope.currentNode.linkedFrom.length > 0; }
+		$scope.clusterNamesEnabled = function() { return $scope.clusterNames != null && $scope.clusterNames.length > 0; }
 
+		//funzione che abilita l'update
+		$scope.updateEnabled = function() {
+			return $scope.cluster  &&
+				$scope.cluster._id &&
+				$scope.cluster._rev;
+		}
+		$scope.clusterEnabled = function() { return $scope.cluster != null && !$scope.updateEnabled(); }		
+
+		$scope.loadClusterNames= function() {
+			$http({
+	            method: 'GET',
+	            //url: $scope.DBURLNAMES,
+				url: $scope.BASEURL+"/clusterNames",
+	            dataType: 'jsonp',
+	            headers: {'Accept': 'application/json'}
+	         })
+			.success (function (data) {
+				console.log("Cluster names loaded")
+				$scope.clusterNames = data;
+				$scope.searchClusterNames = { "names" : $scope.clusterNames};
+				console.log (data);
+			})
+			.error (function (data,status) {
+				console.log("Cluster names NOT loaded")
+				console.log (data);
+				console.log (status);
+			});		
+
+		}
+
+		$scope.loadClusterNames();
+
+		$scope.deleteCluster = function() {
+			var my_data = $scope.cluster;
+			swal ({
+				title : "Are you sure?",
+				text  : "Do you want to delete the current cluster on the DB?",
+				showCancelButton : true,
+				confirmButtonColor : "#DD6B55",
+				confirmButtonText : "Yes, delete it!"},
+				function () {
+				$http({
+					method: 'DELETE',
+					url: $scope.BASEURL+"/"+my_data._id+"/"+my_data._rev,
+					data_type: 'jsonp'
+				})
+	    		.then (function (data) {
+					console.log ("Data ok")
+					$scope.message = "Data inserted";
+					$scope.post_data = data;
+					console.log(data);	
+					$scope.loadClusterNames();
+					swal("","Cluster "+ $scope.cluster.clusterName + " deleted!", "success");
+				},
+				function (data) {
+					console.log ("Data ko")
+					$scope.message = "Insertion failed";
+					$scope.post_data = data;
+					console.log (data);				
+					swal("","Cluster "+ $scope.cluster.clusterName + " NOT deleted!", "error");
+				});		
+			});
+		}
+
+
+		$scope.updateCluster = function() {			
+			var my_data = $scope.cluster;
+			swal ({
+				title : "Are you sure?",
+				text  : "Do you want to update the current cluster on the DB?",
+				showCancelButton : true,
+				confirmButtonColor : "#DD6B55",
+				closeOnConfirm: true,
+				confirmButtonText : "Yes, update it!"
+				},
+				function () {
+					$http({
+						method: 'PUT',
+						url: $scope.BASEURL,
+						data: my_data,
+						data_type: 'jsonp'
+					})
+		    		.then (function (data) {
+						console.log ("Data ok")
+						$scope.message = "Data inserted";
+						$timeout (function()  {
+							swal("","Cluster "+ $scope.cluster.clusterName + " updated!", "success");
+						});
+						$scope.post_data = data;
+						console.log(data);	
+						//$scope.loadClusterNames();
+
+					},
+					function (data) {
+						console.log ("Data ko")
+						$scope.message = "Insertion failed";
+						$scope.post_data = data;
+						console.log (data);				
+						$timeout (function()  {
+							swal("","Cluster "+ $scope.cluster.clusterName + " NOT updated!", "error");
+						});
+					});		
+				});
+		}
 
 		$scope.saveToCouch = function () {
 			var my_data = $scope.cluster;
 			$http({
 				method: 'POST',
-				url: 'http://localhost:41459/diaspora',
+				url: $scope.BASEURL,
 				data: my_data,
 				data_type: 'jsonp'
 			})
@@ -22,6 +145,7 @@ var diasporaApp = angular.module("diasporaApp",[])
 				$scope.message = "Data inserted";
 				$scope.post_data = data;
 				console.log(data);	
+				$scope.loadClusterNames();
 			},
 			function (data) {
 				console.log ("Data ko")
@@ -34,13 +158,16 @@ var diasporaApp = angular.module("diasporaApp",[])
 		$scope.getClusterFromServer = function () {
 			$http({
 	            method: 'GET',
-	            url: 'http://localhost:41459/diaspora',
+				url: $scope.BASEURL,
 	            dataType: 'jsonp',
 	            headers: {'Accept': 'application/json'}
 	         })
 			.success (function (data) {
+				console.log (data);
 				$scope.currentNode = null;
 				$scope.cluster = data;
+				swal("Cluster created ","Cluster "+ data.clusterName + " has been succesfully created!", "success");
+
 			})
 			.error (function (data,status) {
 				console.log (data);
@@ -58,4 +185,38 @@ var diasporaApp = angular.module("diasporaApp",[])
 			} else
 				$scope.currentNode = null;
 		}		
+
+
+		$scope.loadClusterFromDlg = function () {
+			$("#loadClusters").modal('hide');
+			console.log ("loading selected cluster from dialog");
+			console.log ($scope.selectedClusterId);
+			$scope.loadClusterFromService($scope.selectedClusterId, $scope);
+
+		}
+
+		$scope.loadClusterFromService = function (id, theScope) {
+			console.log ("Loading id")
+			$http({
+	            method: 'GET',
+				url: $scope.BASEURL+"/"+id+"/-1",
+	            dataType: 'jsonp',
+	            headers: {'Accept': 'application/json'}
+	         })
+			.success (function (data) {
+				console.log ("Cluster loaded")
+				$scope.currentNode = null;
+				$scope.cluster = data;
+				swal("","Cluster "+ data.clusterName + " has been loaded", "success");
+				console.log (data);
+			})
+			.error (function (data,status) {
+				console.log ("Cluster NOT loaded")
+				console.log (data);
+				console.log (status);
+				swal("Error","Problems loading cluster " + id,"error");
+			});		
+		}
 	});
+
+
